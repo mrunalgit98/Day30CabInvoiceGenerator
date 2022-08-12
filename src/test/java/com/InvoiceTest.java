f1ea1b549bd8388c97b8cde94388c1177da50788
mrunalgit98 <88592639+mrunalgit98@users.noreply.github.com>
2022-08-12 22:27:11 +0530

    Update InvoiceTest.java

--- src/test/java/com/InvoiceTest.java
+++ src/test/java/com/InvoiceTest.java
@@ -2,47 +2,21 @@ package com;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class InvoiceTest {
 
-    InvoiceGenerator invoiceGenerator;
-
-
-    @Test
-    public void givenDistanceAndMinute_shouldReturn_totalFare_forMultipleRides() {
-        Ride [] ride = {
-                new Ride(20.0, 15),
-                new Ride(0.3, 2),
-                new Ride(5.0, 3)
-        };
-        double fare = invoiceGenerator.calculateFare(ride);
-        assertEquals(273, fare,  0.0);
-    }
+   
 
     @Test
-    public void givenDistanceAndMinute_shouldReturntotalFare_forMultipleRides() {
-        Ride [] ride = {
-                new Ride(2.0, 15),
-                new Ride(0.5, 1),
-                new Ride(50.0, 30)
-        };
-        double fare = invoiceGenerator.calculateFare(ride);
-        assertEquals(571, fare,  0.0);
-    }
-
-
-
-    @Test
-    public void givenMultipleRides_shouldReturn_InvoiceSummary() {
-        Ride [] ride = {
-                new Ride(2.0, 14),
-                new Ride(0.5, 1),
-                new Ride(50.0, 30)
-        };
-        InvoiceSummary invoiceSummary = invoiceGenerator.calculateTotalFare(ride);
-        InvoiceSummary expected = new InvoiceSummary(3, 570, 190);
-        assertEquals(expected, invoiceSummary);
+    public void invoiceGenerator_ShouldReturnInvoiceSummary() {
+        InvoiceGenerator invoice = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5), new Ride(0.1, 1)};
+        double totalFare = invoice.calculateFare(rides);
+        InvoiceSummary actualInvoiceSumarry = new InvoiceSummary(totalFare, rides.length);
+        InvoiceSummary expectedInvoiceSumarry = new InvoiceSummary(30.0, 2);
+        Assert.assertEquals(expectedInvoiceSumarry,actualInvoiceSumarry);
     }
 
 }
