04c3233a5b2a6a8c736e0c6b140ca80c9dd42381
Resh@308 <reshsingh1995@gmail.com>
2022-08-12 23:01:11 +0530

    Added: Step 5 Premium rides added

--- src/main/java/com/CabInvoiceGenerator.java
+++ src/main/java/com/CabInvoiceGenerator.java
@@ -1,49 +1,60 @@
 package com;
 
-
 public class CabInvoiceGenerator {
-    private static final double COST_PER_KM = 10.0;
-    private static final int COST_PER_TIME = 1;
-    private static final int MINIMUM_FARE = 5;
+    private static double COST_PER_KM;
+    private static  int COST_PER_TIME ;
+    private static  int MINIMUM_FARE ;
     private RideRepository rideRepository;
-    public double calculateFare(double distance, double time) {
+
+    public enum RideMode {
+        NORMAL(10,
+                1,
+                5),
+        PREMIUM(15,
+                2,
+                20.0);
+
+        private double COST_PER_KM;
+        private int COST_PER_TIME;
+        private double MINIMUM_FARE;
+
+        RideMode(double MINIMUM_COST_PER_KM, int COST_PER_MIN, double MINIMUM_FARE) {
+            this.COST_PER_KM = MINIMUM_COST_PER_KM;
+            this.COST_PER_TIME = COST_PER_MIN;
+            this.MINIMUM_FARE = MINIMUM_FARE;
+        }
+    }
+
+    public double calculateFare(double distance, double time, double COST_PER_KM,int COST_PER_TIME,double MINIMUM_FARE ) {
         double calculate_Fare = distance * COST_PER_KM + time * COST_PER_TIME;
         if (calculate_Fare < MINIMUM_FARE) {
             return MINIMUM_FARE;
         } else
             return calculate_Fare;
     }
+
     public CabInvoiceGenerator() {
         this.rideRepository = new RideRepository();
     }
 
-    public double calculateFare(Ride[] rides) {
-        double totalFare = 0.0;
+    public InvoiceSummarry calculateFare(Ride[] rides) {
+        double totalFare =0;
         for (Ride ride : rides) {
-            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+            totalFare += calculateFare(ride.getDistance(), ride.getTime(),ride.rideMode.COST_PER_KM,ride.rideMode.COST_PER_TIME,ride.rideMode.MINIMUM_FARE);
         }
-        return totalFare;
+
+        return new InvoiceSummarry(totalFare,rides.length);
     }
-    public InvoiceSumarry calculateFare(Ride[] rides, String type) {
-        double totalFare = 0;
-        if (type == "normal") {
-            for (Ride ride : rides) {
-                totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
-            }
-            return new InvoiceSumarry(totalFare, rides.length);
-        } else {
-            for (Ride ride : rides) {
-                totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
-            }
-            return new InvoiceSumarry(totalFare, rides.length);
-        }
-    }
+
+
     public void addRides(String userId, Ride[] rides) {
         rideRepository.addRide(userId, rides);
     }
 
-    public InvoiceSumarry getInvoiceSummary(String userId) {
-        return this.calculateFare( rideRepository.getRides(userId), "normal");
+    public InvoiceSummarry getInvoiceSummary(String userId) {
+        return this.calculateFare(rideRepository.getRides(userId));
     }
+
+
 
 }
